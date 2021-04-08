e7a130e25cd002ec0a7e92d9a52d53a7766a16cc
Amir Mohamad Mohamadi <amirmm5560@gmail.com>
2021-04-08 14:17:19 +0330

    dorost kardan cart.selector and user.selector baraye mapstatetoprops and import createstructuredselector baraye stat haye mapstatetoprops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/home-page/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -8,6 +9,7 @@ import Header from './pages/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import './App.css';
 
@@ -51,8 +53,8 @@ class App extends React.Component {
   }
 };
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
