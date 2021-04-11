966a792a6c12f118c64222e987a6ed90cc37e9d6
Amir Mohamad Mohamadi <amirmm5560@gmail.com>
2021-04-12 02:53:25 +0330

    masir yabie pishrafteh baraye shop page masalan age ro kolah klik kardim khodesho neshon bede

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-perview/collection-preview.component';
+import CollectionPreview from '../collection-perview/collection-preview.component';
 import { selectCollections } from '../../redux/shop/shop.selector';
 
 import './collection-overview.styles.scss';
